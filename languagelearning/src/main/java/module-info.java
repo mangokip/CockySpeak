7081540125eb3b9ac0bdf5028db56d55c6c256c4
CarsonS21 <sessomsc@email.sc.edu>
2024-11-14 09:03:08 -0500

    added needed requirements to module.info

--- languagelearning/src/main/java/module-info.java
+++ languagelearning/src/main/java/module-info.java
@@ -1,6 +1,19 @@
 module com.language {
     requires javafx.controls;
     requires javafx.fxml;
+    requires junit;
+    requires json.simple;
+    requires jlayer;
+    requires software.amazon.awssdk.core;
+    requires software.amazon.awssdk.services.polly;
+    requires software.amazon.awssdk.regions;
+    requires software.amazon.awssdk.utils;
+    requires org.slf4j;
+    requires org.slf4j.simple;
+    requires software.amazon.awssdk.awscore;
+    requires software.amazon.eventstream;
+
+
 
     opens com.language to javafx.fxml;
     exports com.language;
