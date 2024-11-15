6471d31d1495522cd827568aca801e031a197867
dd010504 <dd26@email.sc.edu>
2024-11-14 21:56:22 -0500

    fixed maven

--- languagelearning/src/main/java/module-info.java
+++ languagelearning/src/main/java/module-info.java
@@ -1,19 +1,15 @@
 module com.language {
     requires javafx.controls;
     requires javafx.fxml;
-    requires junit;
     requires json.simple;
     requires jlayer;
     requires software.amazon.awssdk.core;
     requires software.amazon.awssdk.services.polly;
     requires software.amazon.awssdk.regions;
     requires software.amazon.awssdk.utils;
-    requires org.slf4j;
-    requires org.slf4j.simple;
+    requires org.slf4j; // Only require org.slf4j
     requires software.amazon.awssdk.awscore;
     requires software.amazon.eventstream;
-
-
 
     opens com.language to javafx.fxml;
     exports com.language;
